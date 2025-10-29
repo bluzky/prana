a33b1b415a7aa7073b8f1b55ea90a918bf6cc4e8
Always blue <bluesky.1289@gmail.com>
2025-10-29 15:34:50 +0700

    Update deps

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Prana.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mau, "~> 0.4"},
+      {:mau, "~> 0.6"},
       {:nestex, "~> 0.2"},
       {:req, "~> 0.5"},
       {:skema, "~> 1.4"},
